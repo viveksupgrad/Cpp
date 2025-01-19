bf86441a2596b55f634c39ff44eb20a4f18152b9
Vivek Kumar Singh <vivekkrsingh.mail@gmail.com>
2025-01-20 02:42:58 +0530

    Update bubbleSort.cpp

--- concepts/sorting/bubbleSort.cpp
+++ concepts/sorting/bubbleSort.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 using namespace std;
 
-void swap(int x, int y){
-    
-    int temp = x;
-    x = y;
-    y = temp;
 
-}
-void bubbleSort(int arr[], int n){
-    bool swapped;
-
-    for(int i=0; i<n-1; i++){
-        swapped = false;
-
-        for(int j = 0; j < n-i-1; j++){
-            if (arr[j] > arr[j+1]){
-                swap(arr[j], arr[j+1]);
-                swapped = true;
-            }  
-            }
-         if(!swapped){
-        break;
-    }
-    }
+//time complexity O(n^2)
+void bubbleSort(int arr[], int n)
+{
     for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-        
-    }
-    
+        for(int j=0; j<n-i-1; j++ ){
+            if(arr[j]>arr[j+1]){
+                //int temp = arr[j];
+                //arr[j] = arr[j+1];
+                //arr[j+1] = temp;
+
+
+                swap(arr[j], arr[j+1]);
+            }
+
+        }
     }
 
-int main(){
-    int arr[] = {1,2,9, 11, 3};
-    
-    for(int i=0; i<5; i++){
-        cout<<arr[i]<<" ";
-        
+  
+}
+//optimisation
+
+
+
+int main()
+{
+    cout<<"enter array size"<<endl;
+    int n;
+    cin>>n;
+    int arr[n];
+    cout<<"enter array elements"<<endl;
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
     }
-    cout<<endl;
-    bubbleSort(arr, 5);
+    cout<<"raw array"<<endl;
+
+     for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+  cout<<endl;
+  bubbleSort(arr, n);
+  cout<<endl;
+  cout<<"sorted array"<<endl;  
+  for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
 }
