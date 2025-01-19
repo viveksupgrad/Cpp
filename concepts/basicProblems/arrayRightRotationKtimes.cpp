83b932361fd6c340bd77cad8c94b9fbead684b50
Vivek Kumar Singh <vivekkrsingh.mail@gmail.com>
2025-01-19 22:15:32 +0530

    Create arrayRightRotationKtimes.cpp
    Given an Array of size N and a value K, around which we need to right rotate the array. The program should print the array after K rotation towards the right.

---
+++ concepts/basicProblems/arrayRightRotationKtimes.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Function to rightRotate array
+void RightRotate(int a[], int n, int k)
+{
+    
+    // If rotation is greater 
+    // than size of array
+    k = k % n;
+
+    for(int i = 0; i < n; i++)
+    {
+       if(i < k)
+       {
+           
+           // Printing rightmost 
+           // kth elements
+           cout << a[n + i - k] << " ";
+       }
+       else
+       {
+           
+           // Prints array after
+           // 'k' elements
+           cout << (a[i - k]) << " ";
+       }
+    }
+    cout << "\n";
+}
+    
+// Driver code
+int main()
+{
+    int Array[] = { 1, 2, 3, 4, 5 };
+    int N = sizeof(Array) / sizeof(Array[0]);
+    int K = 2;
+    
+    RightRotate(Array, N, K);
+}
